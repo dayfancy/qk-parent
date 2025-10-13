e30bba227212383ef198f551431267d778e45576
ZuoYou <2101822071@qq.com>
2025-10-13 20:38:00 +0800

    feat: MP-插件-分页插件使用

--- qk-management/src/test/java/com/qk/management/service/impl/CourseServiceImplTests.java
+++ qk-management/src/test/java/com/qk/management/service/impl/CourseServiceImplTests.java
@@ -1,6 +1,8 @@
 package com.qk.management.service.impl;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.PageDTO;
 import com.qk.entity.Course;
 import com.qk.management.service.CourseService;
 import org.junit.jupiter.api.Test;
@@ -8,11 +10,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 class CourseServiceImplTests {
     @Autowired
     private CourseService courseService;
+
+    @Test
+    void testPage(){
+        IPage<Course> page = new PageDTO<>(2,2);
+        IPage<Course> result = courseService.page(page);
+        // 获取总记录数
+        long total = result.getTotal();
+        System.out.println(total);
+        long pages = result.getPages();
+        System.out.println(pages);
+        List<Course> records = result.getRecords();
+        for (Course record : records) {
+            System.out.println(record);
+        }
+    }
+
+
+
+
+
+
     @Test
     void testDeleteById() {
         courseService.deleteById(18);
