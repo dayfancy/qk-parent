bed554796f2beb86de2421a04377df66bb7ec97f
ZuoYou <2101822071@qq.com>
2025-10-14 11:54:10 +0800

    fix: 线索管理-线索列表归属人展示问题

--- qk-entity/src/main/java/com/qk/entity/domain/clue/ClueDO.java
+++ qk-entity/src/main/java/com/qk/entity/domain/clue/ClueDO.java
@@ -1,12 +1,19 @@
 package com.qk.entity.domain.clue;
 
 import com.qk.entity.Clue;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @Author: RightSquare
  * @Date: 2025/10/14 11:12
  * @Description:
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClueDO extends Clue {
     private String assignName;
 }
