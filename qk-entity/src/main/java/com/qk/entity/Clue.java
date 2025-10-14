e05d726a0807cdbc9e5d8afc1a0548ecadff8be0
ZuoYou <2101822071@qq.com>
2025-10-14 11:55:37 +0800

    fix: 线索管理-时间格式问题

--- qk-entity/src/main/java/com/qk/entity/Clue.java
+++ qk-entity/src/main/java/com/qk/entity/Clue.java
@@ -1,5 +1,6 @@
 package com.qk.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -39,8 +40,10 @@ public class Clue {
     // 意向等级, 1:近期学习、2:打算学习(考虑中)、3:进行了解、4:打酱油
     private Integer level;
     // 下次跟进时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime nextTime;
     // 创建时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
     // 修改时间
     private LocalDateTime updateTime;
